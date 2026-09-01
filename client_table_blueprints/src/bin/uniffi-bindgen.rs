e75c97d492e187e9d8b5d50c27dc7b0661371071
Zakarias Viinikka <zakariasviinikka@gmail.com>
2026-09-01 08:36:28 +0200

    wip: writing the shared table blueprints the android app and the web client are meant to used
    i also need the structs/enum definitions that are gonna be stored as blobs inside the table. so that the callers can turn the blob into agreed upon types

---
+++ client_table_blueprints/src/bin/uniffi-bindgen.rs
@@ -0,0 +1,5 @@
+// This small binary lets you run `cargo run --bin uniffi-bindgen -- ...`
+// to generate bindings easily.
+fn main() {
+    uniffi::uniffi_bindgen_main()
+}
---
+++ client_table_blueprints/src/lib.rs
@@ -0,0 +1,12 @@
+uniffi::setup_scaffolding!();
+
+/*#[uniffi::export]
+pub fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+#[derive(uniffi::Record)]
+pub struct TableInfo {
+    pub name: String,
+    pub columns: Vec<String>,
+}*/
